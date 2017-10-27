5fe22f15462426c8c65a89e1191cbd58cc3f6d23
leigh schrandt <stealthybox@users.noreply.github.com>
2017-10-27 11:03:31 -0600

    Parameterize Redirect
    uses ENV

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var express = require('express');
 
 var server = express();
 var port = process.env.PORT || 8080;
+var redirect_protocol = process.env.REDIRECT_PROTOCOL || 'https';
+var redirect_domain = process.env.REDIRECT_DOMAIN || 'yourdomain.com';
 var http = require('http');
 
 server.get('*', function(request, response){ 
-  response.redirect(301, 'https://yourdomain.com' + request.url);
+  response.redirect(301, redirect_protocol + '://' + redirect_domain + request.url);
 });
 
 http.createServer(server).listen(port);
